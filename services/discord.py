1811b7a6543512bdff679dfa2de6ba143c6e2bf9
Bialy <21p0068@eng.asu.edu.eg>
2026-07-24 03:32:41 +0300

    modified discored.py

--- services/discord.py
+++ services/discord.py
@@ -1,5 +1,4 @@
 import requests
-
 from config import DISCORD_WEBHOOK
 
 
@@ -8,12 +7,17 @@ def send_notification(changes):
     if not changes:
         return
 
+    print("Webhook loaded:", DISCORD_WEBHOOK is not None)
+
     message = "\n".join(changes)
 
-    requests.post(
+    response = requests.post(
         DISCORD_WEBHOOK,
         json={
             "content": f"🎬 **VOX Booking Update**\n\n{message}"
         },
         timeout=30,
     )
+
+    print("Discord status:", response.status_code)
+    print("Discord response:", response.text)
